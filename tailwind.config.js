dafd7be242ab10424f73fb9e5e9bf10957a54317
Charles Okwuagwu <charleso@paperlesssolutionsltd.com.ng>
2021-02-17 07:11:25 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,46 @@
 const colors = require('windicss/colors')
-const defaultTheme = require('windicss/defaultTheme')
+// const defaultTheme = require('windicss/defaultTheme')
 
 module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans]
-      },
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        black: '#000',
-        white: '#fff',
-        bluegray: colors.blueGray,
-        coolgray: colors.coolGray,
-        gray: colors.gray,
-        truegray: colors.trueGray,
-        warmgray: colors.warmGray,
-        red: colors.red,
-        orange: colors.orange,
-        amber: colors.amber,
-        yellow: colors.yellow,
-        lime: colors.lime,
-        green: colors.green,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        lightblue: colors.lightBlue,
-        blue: colors.blue,
-        indigo: colors.indigo,
-        violet: colors.violet,
-        purple: colors.purple,
-        fuchsia: colors.fuchsia,
-        pink: colors.pink,
-        rose: colors.rose
-      }
+    fontFamily: {
+      sans: ['"Inter var"']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      bluegray: colors.blueGray,
+      coolgray: colors.coolGray,
+      gray: colors.gray,
+      truegray: colors.trueGray,
+      warmgray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightblue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose
     }
   },
   plugins: [
+    require('windicss/plugin/filters'),
     require('windicss/plugin/forms'),
-    require('windicss/plugin/typography'),
+    require('windicss/plugin/aspect-ratio'),
     require('windicss/plugin/line-clamp'),
-    require('windicss/plugin/aspect-ratio')
+    require('windicss/plugin/typography')
   ]
 }
